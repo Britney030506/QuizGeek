c61f600389744723ff788cbebf53cfdc742bab01
Britney <britneypalma16@gmail.com>
2023-05-02 19:22:10 -0600

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: ['src/main.js', './index.html']
+    }
+  }
 })
+
